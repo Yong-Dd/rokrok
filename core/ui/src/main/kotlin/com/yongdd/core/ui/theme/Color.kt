58b7097f2dd6dd46bc68cd17b0de64c5becfb73f
Yong-Dd <padoyj@gmail.com>
2024-07-29 09:17:01 +0900

    color& theme 정리

--- core/ui/src/main/kotlin/com/yongdd/core/ui/theme/Color.kt
+++ core/ui/src/main/kotlin/com/yongdd/core/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.yongdd.core.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+object ColorSet {
+    // main color
+    val green_083D0A6 = Color(0xFF_083D0A6)
+    val yellow_0E0E890 = Color(0xFF_0E0E890)
+}
